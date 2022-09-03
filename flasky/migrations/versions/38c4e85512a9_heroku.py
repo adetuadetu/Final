2c0105d0789dd31fa660ceb01a225fc1ad8ca6fb
Joan Osorio <joan-osorio95@outlook.com>
2022-09-03 15:48:12 +0200

    heroku2

---
+++ flasky/migrations/versions/38c4e85512a9_heroku.py
@@ -0,0 +1,26 @@
+"""heroku
+
+Revision ID: 38c4e85512a9
+Revises: 25b20512432d
+Create Date: 2022-09-01 16:33:40.272916
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38c4e85512a9'
+down_revision = '25b20512432d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
+
