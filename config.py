c80ce8f47c161eb1c33622299cc5c7b78ac10d3c
adetuadetu <jposorio95@hotmail.com>
2022-11-29 06:45:03 +0100

    changes to .env file

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'thisisnotthekey1970'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.office365.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp-relay.sendinblue.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
