64f33bab6c765ce93d6cd0cb0f7eb7ad9f9f894d
adetuadetu <jposorio95@hotmail.com>
2023-01-02 15:37:02 +0100

    db issue

--- app/main/views.py
+++ app/main/views.py
@@ -50,7 +50,7 @@ def index():
         query = current_user.followed_posts
     else:
         query = Post.query
-    pagination = query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
+    pagination = query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
     posts = pagination.items 
     return render_template('index.html', form=form, posts=posts, show_followed=show_followed, pagination=pagination)
 
